23c0efed108962886bb831f471a20f4b2239b401
kanzd <kanu0704@gmail.com>
2021-07-27 21:29:59 +0530

    aaj

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import Home from "./component/Home/index";
+
 function App() {
+  
   return (
     <>
       <BrowserRouter>
         <Route exact path = "/">
           <Home></Home>
         </Route>
-      </BrowserRouter>
+      </BrowserRouter>S
     </>
   );
 }
